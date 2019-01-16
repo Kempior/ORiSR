ff655f7e9ac3471a33d9b374078a3d9154da73df
ExplodingPotato <wojtek4p4@gmail.com>
2019-01-16 23:27:33 +0100

    Changed .NET Framework version to 4.7, added more ways to multiply matrices

--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿#define _DEBUG_PRINT_
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Matrix
@@ -12,9 +13,10 @@ namespace Matrix
 		static void Main(string[] args)
 		{
 			object _ = new object();
-			int N = 3;
+			int N = 2000;
 			Random rand = new Random();
 
+			Console.Write("Generating matrices...\t");
 			double[][] mat1 = new double[N][], mat2 = new double[N][];
 			for (int i = 0; i < N; i++)
 			{
@@ -27,14 +29,17 @@ namespace Matrix
 					mat2[i][j] = rand.NextDouble() * 100;
 				}
 			}
+			Console.WriteLine("Done!\n");
 
 			double[][] result;
 
 			Stopwatch sw = new Stopwatch();
-			
+
+			//=======================================================
 			//Sequential
-			result = MakeMatrixGreatAgain(N);
+			result = CreateResultMatrix(N);
 			sw.Start();
+
 			for (int row = 0; row < N; row++)
 			{
 				for (int i = 0; i < N; i++)
@@ -47,60 +52,117 @@ namespace Matrix
 			}
 
 			sw.Stop();
-			Print(result, sw);
-			
+			Console.WriteLine("Sequential");
+			PrintSw(sw);
+#if _DEBUG_PRINT
+			PrintMatrix(result);
+#endif
+			Console.WriteLine();
+
+			//=======================================================
 			//Parallel For
-			result = MakeMatrixGreatAgain(N);
+			result = CreateResultMatrix(N);
 			sw.Reset();
 			sw.Start();
+
 			Parallel.For(0, N, row =>
 			{
 				for (int i = 0; i < N; i++)
 				{
-					for (int column = 0; column < N; column++)
+					for (int col = 0; col < N; col++)
 					{
-						result[row][column] += mat1[row][i] * mat2[i][column];
+						result[row][col] += mat1[row][i] * mat2[i][col];
 					}
 				}
 			});
-			sw.Stop();
-			Print(result, sw);
 
-			//Parallel For Local
-			result = MakeMatrixGreatAgain(N);
+			sw.Stop();
+			Console.WriteLine("Parallel.For with iteraring over rows (faster than columns because cache).");
+			PrintSw(sw);
+#if _DEBUG_PRINT
+			PrintMatrix(result);
+#endif
+			Console.WriteLine();
+			
+			//=======================================================
+			//Task
+			result = CreateResultMatrix(N);
 			sw.Reset();
 			sw.Start();
 
-			Parallel.For(0, N, () => new double[N], (row, state, local) =>
-			{
-				for (int i = 0; i < N; i++)
-				{
-					for (int column = 0; column < N; column++)
-					{
-						local[row] += mat1[row][i] * mat2[i][column];
-					}
-				}
+			List<Task> tasks = new List<Task>(result.Length);
 
-				return local;
-			}, local =>
+			for (int row = 0; row < result.Length; row++)
 			{
-				lock(_)
-				{
-					Console.WriteLine("Dl" + local.Length);
-					foreach (var item in local)
+				tasks.Add(new Task((object rowObj) => {
+
+					int thisRow = (int)rowObj;
+					for (int i = 0; i < mat2.Length; i++)	// Iterates over every element in row
 					{
-						Console.Write(item + "\t");
+						for (int col = 0; col < result[thisRow].Length; col++)
+						{
+							result[thisRow][col] += mat1[thisRow][i] * mat2[i][col];
+						}
 					}
-					Console.WriteLine();
-				}
-			});
+				}, row));
+
+				tasks[row].Start();
+			}
+
+
+			foreach (Task task in tasks)
+			{
+				task.Wait();
+			}
+
+			sw.Stop();
+			Console.WriteLine("Task");
+			PrintSw(sw);
+#if _DEBUG_PRINT
+			PrintMatrix(result);
+#endif
+			Console.WriteLine();
+
+			//=======================================================
+			//ThreadPool with manual counter
+			result = CreateResultMatrix(N);
+			sw.Reset();
+			sw.Start();
+
+			int remainingRows = result.Length;
+			for (int row = 0; row < result.Length; row++)
+			{
+				ThreadPool.QueueUserWorkItem((object rowObj) => {
+
+					int thisRow = (int)rowObj;
+					for (int i = 0; i < mat2.Length; i++)	// Iterates over every element in row
+					{
+						for (int col = 0; col < result[thisRow].Length; col++)
+						{
+							result[thisRow][col] += mat1[thisRow][i] * mat2[i][col];
+						}
+					}
+
+					lock (_)
+						remainingRows--;
+				}, row);
+			}
+			while (remainingRows > 0)
+				Thread.Sleep(1);
+
+			sw.Stop();
+			Console.WriteLine("Threadpool with manual counter");
+			PrintSw(sw);
+#if _DEBUG_PRINT
+			PrintMatrix(result);
+#endif
+			Console.WriteLine();
 
 			Console.ReadLine();
 		}
 
-		static void Print(double[][] matrix, Stopwatch sw)
+		static void PrintMatrix(double[][] matrix)
 		{
-			Console.WriteLine("Time: " + sw.ElapsedMilliseconds + " ms");
 			if(matrix.GetLength(0) < 20)
 			{
 				foreach (var row in matrix)
@@ -114,7 +176,12 @@ namespace Matrix
 			}
 		}
 
-		static double[][] MakeMatrixGreatAgain(int n)
+		static void PrintSw(Stopwatch sw)
+		{
+			Console.WriteLine("Time: " + sw.ElapsedMilliseconds + " ms");
+		}
+
+		static double[][] CreateResultMatrix(int n)
 		{
 			double[][] matrix = new double[n][];
 			for (int i = 0; i < n; i++)
